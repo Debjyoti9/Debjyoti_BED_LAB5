ecacfb50783718a80919914d1d4979122589a989
Debjyoti Roy <debjyotiroy039@gmail.com>
2023-04-05 09:46:27 +0530

    Delete gl/gl directory
    done

--- gl/gl/src/main/java/com/example/gl/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gl.repository;
-
-import com.example.gl.domain.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
